727831607d7f913ddb8b676f57b4a35daccec8f7
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-22 21:57:55 -0300

    fix: name

--- Prática/exA_Paulo.py
+++
@@ -1,4 +0,0 @@
-print('Programa de conversão: Celsius para Fahrenheit ')
-cels = float(input('Digite a temperatura em graus Celsius: '))
-fahr = (cels * 1.8) + 32
-print('A temperatura em Fahrenheit é {:.2F} graus.' .format(fahr))
