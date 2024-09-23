134bc7286d6eb5ae9fd83d7e5c24039263273b4a
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-22 21:57:09 -0300

    refactor: change folder

--- Fatorial.py
+++
@@ -1,4 +0,0 @@
-from math import factorial
-
-for i in range(1,11,2):
-    print(factorial(i))
---
+++ Prática/Fahrenheit.py
@@ -0,0 +1,4 @@
+print('Programa de conversão: Celsius para Fahrenheit ')
+cels = float(input('Digite a temperatura em graus Celsius: '))
+fahr = (cels * 1.8) + 32
+print('A temperatura em Fahrenheit é {:.2F} graus.' .format(fahr))
