e83f2820a80912f92622ca66a419de45d1933c23
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-25 00:05:47 -0300

    fix: change for Fundamental folder

---
+++ Fundamental/Divisivel.py
@@ -0,0 +1,5 @@
+print('-+' * 19)
+print('Todos divisíveis por 4 menores que 200')
+print('-+' * 19)
+for i in range(4,200,4):
+    print(i, end=' ')
---
+++ Fundamental/Fatorial.py
@@ -0,0 +1,4 @@
+from math import factorial
+
+for i in range(1,11,2):
+    print(factorial(i))
